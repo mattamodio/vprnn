Fixed numerical instability from softmax-crossentropy and added other stuff

NWLSTM/NWLSTM_Net.py
@@ -4,6 +4,7 @@ import theano
 from theano import tensor as T, function, printing
 from utils import *
 from NWLSTM_Layer import NWLSTM_Layer
+from theano.compile.nanguardmode import NanGuardMode
 
 class NWLSTM_Net:
     def __init__(self, word_dim, hidden_dim=100, minibatch_dim=1, bptt_truncate=4, num_layers=1,
@@ -73,10 +74,10 @@ class NWLSTM_Net:
         y = T.tensor3('y', dtype=theano.config.floatX)
         learning_rate = T.scalar('learning_rate', dtype=theano.config.floatX)
         
-        sequence_length = 10
+        sequence_length = 7
         x.tag.test_value = np.random.uniform(0, 1, size=(sequence_length,self.word_dim,self.minibatch_dim)).astype(theano.config.floatX)
         y.tag.test_value = np.random.uniform(0, 1, size=(sequence_length,self.word_dim,self.minibatch_dim)).astype(theano.config.floatX)
-        learning_rate.tag.test_value = .01
+        learning_rate.tag.test_value = .001
         #########################################################
         #########################################################
         # Scan functions
@@ -114,8 +115,8 @@ class NWLSTM_Net:
             for i,layer in enumerate(self.layers):
                 layer_input = layer.forward_prop(layer_input, is_push, is_pop)
                 layer_input = layer_input*masks[:,:,i] # dropout
-
-            o_t = T.transpose( T.nnet.softmax( T.transpose(self.W_hy.dot(layer_input/self.softmax_temperature)) ) )
+            
+            o_t = self.W_hy.dot(layer_input)
             
             return o_t
         #########################################################
@@ -134,9 +135,22 @@ class NWLSTM_Net:
         #########################################################
         #########################################################
         # Error calculation and model interface functions
-        o_error = T.sum(T.nnet.categorical_crossentropy(o, y))
-        
-        self.forward_propagation = theano.function([x], o)
+        # comput softmax in numerically stable, column-wise way
+        # move distribution axis to the end, collapse sequences/minibatches along first axis, calculate softmax
+        # for each of the sequence_length*num_minibatches rows, then re-roll and swap axes back
+        swapped_o = T.swapaxes(o,1,2)
+        swapped_flat_o = swapped_o.reshape((-1,swapped_o.shape[-1]))
+        swapped_flat_o = np.exp(swapped_flat_o/self.softmax_temperature)
+        swapped_flat_o = swapped_flat_o / swapped_flat_o.sum(axis=1, keepdims=True)
+        swapped_o = swapped_flat_o.reshape(swapped_o.shape)
+        softmaxed_o = T.swapaxes(swapped_o,1,2)
+
+        # clip softmaxed probabilites to avoid explosion/vanishing during crossentropy calculation
+        clipped_softmaxed_o = T.clip(softmaxed_o, 1e-6, 1 - 1e-6)
+        #o_error = T.sum(T.nnet.categorical_crossentropy(clipped_softmaxed_o[-1,:,:], y[-1]))
+        o_error = T.sum(T.nnet.categorical_crossentropy(clipped_softmaxed_o,y))
+
+        self.forward_propagation = theano.function([x], softmaxed_o)
         self.ce_error = theano.function([x, y], o_error)
         ###########################################################################
         ###########################################################################
@@ -149,7 +163,7 @@ class NWLSTM_Net:
                 updates.append(update)
             return updates
 
-        def RMSprop(cost, params, learning_rate=0.001, b1=0.9, b2=0.999, epsilon=1e-6):
+        def RMSprop(cost, params, learning_rate=0.01, b1=0.9, b2=0.999, epsilon=1e-6):
             grads = T.grad(cost=cost, wrt=params, consider_constant=[masks])
             updates = []
             for p, g in zip(params, grads):
@@ -159,15 +173,15 @@ class NWLSTM_Net:
                 m = b1*m_old + (1-b1)*g
                 v = b2*v_old + (1-b2)*(g**2)
 
-                update = p - (learning_rate*m) / (T.sqrt(T.sqrt(v)) + epsilon)
-
+                update = (learning_rate*m) / (T.sqrt(v) + epsilon)
+                
                 # For bias parameters, set update to be broadcastable along minibatch dimension
                 if p.broadcastable[1]:
                     update = T.addbroadcast(update, 1)
 
                 updates.append((m_old, m))
                 updates.append((v_old, v))
-                updates.append((p, update))
+                updates.append((p, p-update))
 
             return updates
 
@@ -176,15 +190,10 @@ class NWLSTM_Net:
 
         self.train_model = theano.function(inputs=[x,y,learning_rate],
                                       outputs=[],
-                                      updates=updates)
+                                      updates=updates)#, mode=NanGuardMode(nan_is_error=True,inf_is_error=True,big_is_error=True))
+                                      
         ###########################################################################
         ###########################################################################
 
-    def calculate_total_loss(self, X, Y):
-
-        return np.sum([self.ce_error(x,y) for x,y in zip(X,Y)])
-    
-    def calculate_loss(self, X, Y):
-        # Divide calculate_loss by the number of words
-        num_words = np.sum([len(y) for y in Y])
-        return self.calculate_total_loss(X,Y)/float(num_words)
+    def loss_for_minibatch(self, X, Y):
+        return self.ce_error(X,Y)/1.

NWLSTM/generate_from_NWLSTM.py
@@ -5,13 +5,14 @@ import numpy as np
 from datetime import datetime
 from utils import *
 
-DATAFILE = "saved_model_parameters/lstm_rmsprop-theano-512-79-2016-03-04-08-01-53.npz"
-sample_from_distribution = True
-SAMPLE_LIMIT = 299
-SOFTMAX_TEMPERATURE = .7
+DATAFILE = "saved_model_parameters/NWLSTM_savedparameters_22069.0__03-06___14-12-16.npz"
+SAMPLE_FROM_DISTRIBUTION = True
+SAMPLE_LIMIT = 300
+SOFTMAX_TEMPERATURES = np.linspace(.1, 1.1, 4)
 NUM_SENTENCES = 1
 
-STARTING_STRING = "That is one small step for man, one small step for"
+STARTING_STRING = ''' CHAPTER I
+    "Well, '''
 
 # import theano
 # theano.config.optimizer = 'None'
@@ -26,22 +27,21 @@ def one_hot(x, dimensions):
     tmp[x] = 1
     return tmp.reshape(1, dimensions, 1)
 
-def generate_sentence(model):
+def generate_sentence(model, sample_limit=100, sample_from_distribution=True):
     char_to_code_dict, code_to_char_dict, word_dim = model.char_to_code_dict, model.code_to_char_dict, model.word_dim
 
     # We start the sentence with the start token
-    new_sentence = one_hot(char_to_code_dict['LINE_START'], word_dim)
-    for c in STARTING_STRING:
+    new_sentence = one_hot(char_to_code_dict[STARTING_STRING[0]], word_dim)
+    for c in STARTING_STRING[1:]:
         c_one_hot = one_hot(char_to_code_dict[c], word_dim)
         new_sentence = np.vstack((new_sentence,c_one_hot))
 
     # Repeat until we get an end token or reach the sample limit
     sampled_letter = None
-    while len(new_sentence)<SAMPLE_LIMIT:
+    while len(new_sentence)<sample_limit:
 
         next_char_probs = model.forward_propagation(new_sentence) # get probability of next character
-        next_char_probs = [_[0] for _ in next_char_probs[-1][:]]
-        
+        next_char_probs = [_[0] for _ in next_char_probs[-1][:]]        
         
         if sample_from_distribution: # either sample from the distribution or take the most likely next character
             samples = np.random.multinomial(1, next_char_probs)
@@ -49,15 +49,14 @@ def generate_sentence(model):
         else:
             sampled_letter = np.argmax(next_char_probs)
 
-        #if sampled_letter not in code_to_char_dict: continue
+        if sampled_letter not in code_to_char_dict: continue
 
         sampled_one_hot = one_hot(sampled_letter, word_dim) # convert code to one-hot
         new_sentence = np.vstack((new_sentence,sampled_one_hot)) # stack this one-hot onto sentence thus far
 
 
-        if len(new_sentence)%25==0: print len(new_sentence) # print updates every 25 chars
-        if len(new_sentence)%100==0: print "\n" + "".join([code_to_char_dict[np.argmax(x)] for x in new_sentence]) + "\n" # print the sentence so far every 100 chars
-        if code_to_char_dict[sampled_letter] == 'LINE_END': break #if we find the line_end token
+        #if len(new_sentence)%25==0: print len(new_sentence) # print updates every 25 chars
+        #if len(new_sentence)%100==0: print "\n" + "".join([code_to_char_dict[np.argmax(x)] for x in new_sentence]) + "\n" # print the sentence so far every 100 chars
 
 
     sentence_str = [code_to_char_dict[np.argmax(x)] for x in new_sentence]
@@ -67,14 +66,16 @@ def generate_sentence(model):
 
 
 if __name__=="__main__":
-    model = load_model_parameters_lstm(path=DATAFILE,
-                                        sample=1,
-                                        softmax_temperature=SOFTMAX_TEMPERATURE)
+    for softmax_temp in SOFTMAX_TEMPERATURES:
+        print "\n\nSoftmax temperature: {0}".format(softmax_temp)
+        model = load_model_parameters_lstm(path=DATAFILE,
+                                            sample=1,
+                                            softmax_temperature=softmax_temp)
 
-    for i in range(NUM_SENTENCES):
-        sent = []
-        sent = generate_sentence(model)
-        print "".join(sent)
-        print
+        for i in range(NUM_SENTENCES):
+            sent = []
+            sent = generate_sentence(model, sample_limit=SAMPLE_LIMIT, sample_from_distribution=SAMPLE_FROM_DISTRIBUTION)
+            print "".join(sent)
+            print
 
 

NWLSTM/train_NWLSTM.py
@@ -1,20 +1,17 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys, csv, itertools, operator, os, time, ast
+import sys, csv, itertools, operator, os, time, ast, datetime
 import numpy as np
-from datetime import datetime
 import theano
 import theano.tensor as T
 from utils import *
 from NWLSTM_Net import NWLSTM_Net
 
 # Data parameters
-CHAR_LIMIT = False
-MINIBATCH_SIZE = 50
+MAX_MINIBATCHES = 10
+MINIBATCH_SIZE = 100
 SEQUENCE_LENGTH = 100
-BPTT_TRUNCATE = 20
-LINE_START_TOKEN = "LINE_START"
-LINE_END_TOKEN = "LINE_END"
+BPTT_TRUNCATE = -1
 
 # Stack parameters
 WANT_STACK = False
@@ -30,25 +27,24 @@ ACTIVATION = 'tanh' # tanh or relu
 # Optimization parameters
 OPTIMIZATION = 'RMSprop' # RMSprop or SGD
 LEARNING_RATE = .01
-DROPOUT = .5
-NEPOCH = 100
+DROPOUT = .25
+NEPOCH = 1000
+EVAL_LOSS_AFTER = 1
 
 # Data source parameters
 DATAFILE = '../data/war_and_peace.txt'
 MODEL_FILE = None
-#MODEL_FILE = 'saved_model_parameters/lstm_rmsprop-theano-500-83-2016-02-19-18-13-55.npz'
 
 
-theano.config.optimizer = 'None'
+# theano.config.optimizer = 'None'
 # theano.config.compute_test_value = 'raise'
 # theano.config.exception_verbosity = 'high'
-# #theano.config.profile = True
+# # #theano.config.profile = True
 theano.config.warn_float64 = 'warn'
 theano.config.floatX = 'float32'
-# theano.config.device = 'gpu'
 
 print "Training NWLSTM with parameters:\
-    \nLength of training set: {0}\
+    \nMax Number of Minibatches: {0}\
     \nHidden Layer: {1}\
     \nNumber of Layers: {2}\
     \nStack: {8}\
@@ -56,9 +52,9 @@ print "Training NWLSTM with parameters:\
     \nDropout: {9}\
     \nMinibatch size: {4}\
     \nSequence Length: {5}\
-    \nTruncate gradient:{10}\
+    \nTruncate gradient: {10}\
     \nNumber of epochs: {6}\
-    \nLoading from model: {7}\n".format(CHAR_LIMIT, HIDDEN_DIM, NUM_LAYERS, LEARNING_RATE, MINIBATCH_SIZE,
+    \nLoading from model: {7}\n".format(MAX_MINIBATCHES, HIDDEN_DIM, NUM_LAYERS, LEARNING_RATE, MINIBATCH_SIZE,
         SEQUENCE_LENGTH, NEPOCH, MODEL_FILE, WANT_STACK, DROPOUT, BPTT_TRUNCATE)
 
 def one_hot(x, dimensions):
@@ -66,108 +62,193 @@ def one_hot(x, dimensions):
     tmp[x] = 1
     return tmp.astype('float32')
 
-def train_with_sgd(model, X_train, y_train, learning_rate=0.001, nepoch=1, evaluate_loss_after=1):
-    # We keep track of the losses so we can plot them later
-    losses = []
-    examples_per_epoch = (CHAR_LIMIT - SEQUENCE_LENGTH) / MINIBATCH_SIZE
-    percent_progress_points = set([int((i/20.) * examples_per_epoch) for i in range(1,10)])
-    print "Will print progress at steps: {0}".format(sorted(list(percent_progress_points)))
+# def train_with_sgd(model, X_train, y_train, learning_rate=0.001, nepoch=1, evaluate_loss_after=1000):
+    # # We keep track of the losses so we can plot them later
+    # losses = []
+    # examples_per_epoch = (CHAR_LIMIT - SEQUENCE_LENGTH) / MINIBATCH_SIZE
+    # percent_progress_points = set([int((i/10.) * examples_per_epoch) for i in range(1,10)])
+    # print "Will print progress at steps: {0}".format(sorted(list(percent_progress_points)))
 
-    for epoch in range(nepoch):
-        num_examples_seen = 0
-        percent_progress = 1
+    # for epoch in range(nepoch):
+    #     num_examples_seen = 0
+    #     percent_progress = 1
         
-        if (epoch % evaluate_loss_after == 0): # Optionally evaluate the loss
-            loss = model.calculate_loss(X_train, y_train)
-            losses.append((num_examples_seen, loss))
-            time = time.strftime("%m-%d___%H-%M-%S")
-            print "{0}: Loss after epoch={1}: {2}".format(time, epoch, loss)
-            save_model_parameters_lstm("saved_model_parameters/NWLSTM_savedparameters_{0:.1f}__{1}.npz".format(loss, time), model)
+    #     if (epoch % evaluate_loss_after == 0): # Optionally evaluate the loss
+    #         loss = model.calculate_loss(X_train, y_train)
+    #         losses.append((num_examples_seen, loss))
+    #         time = datetime.datetime.now().strftime("%m-%d___%H-%M-%S")
+    #         print "{0}: Loss after epoch={1}: {2}".format(time, epoch, loss)
+    #         save_model_parameters_lstm("saved_model_parameters/NWLSTM_savedparameters_{0:.1f}__{1}.npz".format(loss, time), model)
 
-        for i in xrange(len(y_train)): # One SGD step
-            model.train_model(X_train[i], y_train[i], learning_rate)
-            num_examples_seen += 1
-            # if num_examples_seen in percent_progress_points:
-            #     print "{0}% finished".format(str(percent_progress)+"0")
-            #     percent_progress+=1
-            #     sys.stdout.flush()
+    #     for i in xrange(len(y_train)): # One SGD step
+    #         model.train_model(X_train[i], y_train[i], learning_rate)
+    #         num_examples_seen += 1
+    #         # if num_examples_seen in percent_progress_points:
+    #         #     print "{0}% finished".format(str(percent_progress)+"0")
+    #         #     percent_progress+=1
+    #         #     sys.stdout.flush()
+
+
+def parseFileForCharacterDicts(filename):
+    with open(filename, 'rb') as f:
+        char_to_code_dict = {}
+        code_to_char_dict = {}
+        ALPHABET_LENGTH = 0
+        while True:
+            chunk = f.read(256)
+            for c in chunk:
+                if c not in char_to_code_dict:
+                    char_to_code_dict[c] = ALPHABET_LENGTH
+                    code_to_char_dict[ALPHABET_LENGTH] = c
+                    ALPHABET_LENGTH+=1
+            if not chunk: break
+
+    #ALPHABET_LENGTH+=1 #dicts are zero-indexed, but we want to create vectors of size one greater
+
+    return char_to_code_dict, code_to_char_dict, ALPHABET_LENGTH
 
 def readFile(filename, dictsFile=None, outputDictsToFile=None):
-    global CHAR_LIMIT
     with open(filename, 'rb') as f:
-        char_to_code_dict = {LINE_START_TOKEN: 0, LINE_END_TOKEN: 1}
-        code_to_char_dict = {0: LINE_START_TOKEN, 1: LINE_END_TOKEN}
-        ALPHABET_LENGTH = 1
+        
+        minibatches_yielded = 0
+        current_minibatch = []
+        current_sequence = []
+        while True:
+            c = f.read(1)
+            if not c: break
 
-        filestring = f.read()
-        filestring = filestring.decode('utf-8')
-        if not CHAR_LIMIT:
-            CHAR_LIMIT = len(filestring)
-        c_list = [LINE_START_TOKEN]
-        for c in filestring:
-            c_list.append(c)
 
-            if c not in char_to_code_dict:
-                ALPHABET_LENGTH+=1
-                char_to_code_dict[c] = ALPHABET_LENGTH
-                code_to_char_dict[ALPHABET_LENGTH] = c
+            current_sequence.append(c)
 
-        c_list.append(LINE_END_TOKEN)
+            if len(current_sequence)>=SEQUENCE_LENGTH:
+                current_minibatch.append(current_sequence) #add this sequence to current minibatch
+                current_sequence = current_sequence[1:]
+                
+                if len(current_minibatch)>=MINIBATCH_SIZE: #next sequence starts a new minibatch
+                    #print [[str(c) for c in seq] for seq in current_minibatch]
+                    yield current_minibatch
+                   
+                    minibatches_yielded+=1
+                    if minibatches_yielded>=MAX_MINIBATCHES: break
+                    
+                    current_minibatch = []
+                    if minibatches_yielded%100==0: print minibatches_yielded
+                    continue
 
-    ALPHABET_LENGTH+=1 #dicts are zero-indexed, but we want to create vectors of size one greater
 
-    return c_list, char_to_code_dict, code_to_char_dict, ALPHABET_LENGTH
+                
+
+def readFileWithUnmatchedLefts(filename, dictsFile=None, outputDictsToFile=None):
+    with open(filename, 'rb') as f:
+        
+        minibatches_yielded = 0
+        current_minibatch = []
+        current_sequence = []
+
+        unmatched_pushes = []
+        pop_buffer = []
+        while True:
+            c = f.read(1)
+            if not c: break
+
+            if c==PUSH_CHAR:
+                push_buffer = current_sequence
+                push_buffer.append('NULL')
+                unmatched_pushes.append(push_buffer)
+                print "PUSH BUFFER: {0}".format(push_buffer)
+            elif c==POP_CHAR:
+                pop_buffer = unmatched_pushes.pop()
+                print "POP BUFFER: {0}".format(pop_buffer)
+            else:
+                print c
+            current_sequence.append(c)
+
+            if len(current_sequence)>=SEQUENCE_LENGTH:
+
+
+                current_sequence_with_prepended_buffer = pop_buffer[:]
+                current_sequence_with_prepended_buffer.extend(current_sequence)
+                current_minibatch.append(current_sequence_with_prepended_buffer)
+
+
+
+                #current_minibatch.append(current_sequence) #add this sequence to current minibatch
+                current_sequence = current_sequence[1:]
+                
+                if len(current_minibatch)>=MINIBATCH_SIZE: #next sequence starts a new minibatch
+                    #print [[str(c) for c in seq] for seq in current_minibatch]
+
+
+                    #yield current_minibatch
+                    print current_minibatch
+                   
+                    minibatches_yielded+=1
+                    if minibatches_yielded>=MAX_MINIBATCHES: break
+                    
+                    current_minibatch = []
+                    continue
+
+        return None,None,None
+
 
 def main():
-    c_list, char_to_code_dict, code_to_char_dict, ALPHABET_LENGTH = readFile(DATAFILE)
-    print char_to_code_dict
-
-    c_list = c_list[:CHAR_LIMIT]
-    c_list.append(LINE_END_TOKEN)
-
-    minibatches = []
-    sentences = []
-
-    i=0
-    while i<len(c_list)-SEQUENCE_LENGTH+1:
-        sentences.append(c_list[i:i+SEQUENCE_LENGTH+1])
-        i+=1
-
-    i=0
-    while i+MINIBATCH_SIZE<len(sentences):
-        minibatches.append( sentences[i:i+MINIBATCH_SIZE] )
-        i+=MINIBATCH_SIZE
+    print "Parsing for set of characters: {0}".format(DATAFILE)
+    char_to_code_dict, code_to_char_dict, ALPHABET_LENGTH = parseFileForCharacterDicts(DATAFILE)
+    print "Found {0} characters: {1}\n".format(len(char_to_code_dict), char_to_code_dict.keys())
 
 
-    X_train = [np.dstack([np.asarray([one_hot(char_to_code_dict[c], ALPHABET_LENGTH) for c in sent[:-1]]) for sent in minibatch]) for minibatch in minibatches]
-    y_train = [np.dstack([np.asarray([one_hot(char_to_code_dict[c], ALPHABET_LENGTH) for c in sent[1:]]) for sent in minibatch]) for minibatch in minibatches]
+    #char_to_code_dict, code_to_char_dict, ALPHABET_LENGTH = readFileWithUnmatchedLefts(DATAFILE)
+    #sys.exit()
 
 
-    print "\nDims of one minibatch: {0}".format(X_train[0].shape)
-
-    push_vec = one_hot(char_to_code_dict[PUSH_CHAR], ALPHABET_LENGTH).reshape((ALPHABET_LENGTH,1))
-    pop_vec = one_hot(char_to_code_dict[POP_CHAR], ALPHABET_LENGTH).reshape((ALPHABET_LENGTH,1))
-
-    t1 = time.time()
-    model = NWLSTM_Net(word_dim=ALPHABET_LENGTH, hidden_dim=HIDDEN_DIM, minibatch_dim=MINIBATCH_SIZE, bptt_truncate=BPTT_TRUNCATE,
-                       num_layers=NUM_LAYERS, optimization=OPTIMIZATION, activation=ACTIVATION, want_stack=WANT_STACK,
-                       stack_height=STACK_HEIGHT, push_vec=push_vec, pop_vec=pop_vec, dropout=DROPOUT)
-    t2 = time.time()
-    print "Compiling model took: {0:.0f} seconds\n".format(t2 - t1)
-
-
-    t1 = time.time()
-    model.train_model(X_train[0], y_train[0], LEARNING_RATE)
-    t2 = time.time()
-    print "One SGD step took: {0:.2f} milliseconds".format((t2 - t1) * 1000.)
-
-    model.char_to_code_dict = char_to_code_dict
-    model.code_to_char_dict = code_to_char_dict
-
+    print "Compiling model..."
     if MODEL_FILE != None:
         model = load_model_parameters_lstm(MODEL_FILE, minibatch_dim=MINIBATCH_SIZE)
+    else:
+        push_vec = one_hot(char_to_code_dict[PUSH_CHAR], ALPHABET_LENGTH).reshape((ALPHABET_LENGTH,1))
+        pop_vec = one_hot(char_to_code_dict[POP_CHAR], ALPHABET_LENGTH).reshape((ALPHABET_LENGTH,1))
+        t1 = time.time()
+        model = NWLSTM_Net(word_dim=ALPHABET_LENGTH, hidden_dim=HIDDEN_DIM, minibatch_dim=MINIBATCH_SIZE, bptt_truncate=BPTT_TRUNCATE,
+                           num_layers=NUM_LAYERS, optimization=OPTIMIZATION, activation=ACTIVATION, want_stack=WANT_STACK,
+                           stack_height=STACK_HEIGHT, push_vec=push_vec, pop_vec=pop_vec, dropout=DROPOUT)
+        t2 = time.time()
+        model.char_to_code_dict = char_to_code_dict
+        model.code_to_char_dict = code_to_char_dict
+        print "Finished! Compiling model took: {0:.0f} seconds\n".format(t2 - t1)
 
-    train_with_sgd(model, X_train, y_train, nepoch=NEPOCH, learning_rate=LEARNING_RATE)
+
+    losses=[]
+    for epoch in xrange(NEPOCH):
+        #print "Epoch: {0}".format(epoch)
+        first = True
+        for minibatch in readFile(DATAFILE):
+            #print "Current minibatch: {0}".format([[str(c) for c in seq] for seq in minibatch])
+
+            x = np.dstack([np.asarray([one_hot(char_to_code_dict[c], ALPHABET_LENGTH) for c in seq[:-1]]) for seq in minibatch])
+            y = np.dstack([np.asarray([one_hot(char_to_code_dict[c], ALPHABET_LENGTH) for c in seq[1:]]) for seq in minibatch])
+
+
+            if first and epoch%EVAL_LOSS_AFTER==0:
+                if epoch==0: print "Dims of one minibatch: {0}".format(x.shape)
+                t1 = time.time()
+                model.train_model(x, y, LEARNING_RATE)
+                t2 = time.time()
+                if epoch==0: print "One SGD step took: {0:.2f} milliseconds\n".format((t2 - t1) * 1000.)
+
+
+                loss = model.loss_for_minibatch(x,y)
+                losses.append((epoch, loss))
+                dt = datetime.datetime.now().strftime("%m-%d___%H-%M-%S")
+                print "{0}: Loss on first minibatch after epoch={1}: {2:.1f}".format(dt, epoch, loss)
+                save_model_parameters_lstm("saved_model_parameters/NWLSTM_savedparameters_{0:.1f}__{1}.npz".format(loss, dt), model)
+                first = False
+            else:
+                model.train_model(x, y, LEARNING_RATE)
+
+
+    print "{0}: Loss after all epochs: {1:.1f}".format(dt, loss)
+    save_model_parameters_lstm("saved_model_parameters/NWLSTM_savedparameters_{0:.1f}__{1}.npz".format(loss, dt), model)          
+
 
 
 if __name__=="__main__":